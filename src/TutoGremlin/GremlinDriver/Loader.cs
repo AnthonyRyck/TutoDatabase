a3380b2a3f276033d11a7cc74a937e828d7a3578
Anthony RYCK <anthony.ryck@hotmail.fr>
2021-10-11 06:44:13 +0200

    Ajout project console pour Gremlin.Net.Driver

---
+++ src/TutoGremlin/GremlinDriver/Loader.cs
@@ -0,0 +1,42 @@
+﻿using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Traversal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GremlinDriver
+{
+	public class Loader
+	{
+		private GraphTraversalSource GremlinRequest;
+
+		public Loader()
+		{
+			using (var client = new GremlinClient(new GremlinServer("localhost", 8182)))
+			{
+				GremlinRequest = AnonymousTraversalSource.Traversal().WithRemote(new DriverRemoteConnection(client));
+			}
+		}
+
+
+
+		public void Populate()
+		{
+			GremlinRequest.AddV("post")
+				.Property("name", "value")
+				.Property("autreName", "autreValue")
+				.As("p1") // A voir à quoi ça correspond ?
+
+			.AddV("comment")
+				.Property("name", "value")
+				.Property("autreName", "autreValue")
+				.As("c1")
+			.AddE("nameEdge").From("p1").To("c1")
+
+			.Iterate();
+		}
+	}
+}
---
+++ src/TutoGremlin/GremlinDriver/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Traversal;
+
+namespace GremlinDriver
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("##### Lancement de l'application ######");
+			Console.WriteLine("# Appuyer sur une touche pour commencer l'injection.");
+			Console.ReadKey();
+
+
+
+
+
+
+			//string pathBase = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files");
+			//string pathSolarSystem = Path.Combine(pathBase, "systemSolar.json");
+			//string pathJumps = Path.Combine(pathBase, "Jumps.json");
+
+			//Console.WriteLine("Chargement du fichier des systèmes solaires...");
+			//string jsonContentSystems = File.ReadAllText(pathSolarSystem);
+			//List<SolarSystem> allSolarSystems = JsonSerializer.Deserialize<List<SolarSystem>>(jsonContentSystems);
+			//Console.WriteLine($"# Il y a {allSolarSystems.Count} systèmes solaires.");
+
+			//Console.WriteLine("Chargement du fichier des jumps...");
+			//string jsonContentJumps = File.ReadAllText(pathJumps);
+			//List<Jumps> allJumps = JsonSerializer.Deserialize<List<Jumps>>(jsonContentJumps);
+			//Console.WriteLine($"# Il y a {allJumps.Count} sauts interstellaires possibles.");
+
+			//Console.WriteLine("Fin du chargement des fichiers...");
+
+
+			//Console.WriteLine("# Début de la création du Graph !");
+
+			//var gremlinQuerySource = g.ConfigureEnvironment(env => env.UseModel(GraphModel
+			//												.FromBaseTypes<Models.Graph.Vertex, Models.Graph.Edge>(lookup => lookup.IncludeAssembliesOfBaseTypes()))
+			//	.UseJanusGraph(builder => builder
+			//			.AtLocalhost()));
+
+			//LoaderToDocker loadData = new LoaderToDocker(gremlinQuerySource);
+
+			//Console.WriteLine("#--> Drop e la base");
+			//loadData.DropBase().Wait();
+
+			//Console.WriteLine("#--> Création des systèmes (un Vertex / des Vertices !)");
+			//Console.WriteLine("Petite pause.....");
+			//Task.Delay(1000).Wait();
+			//loadData.CreateAllSystems(allSolarSystems).Wait();
+
+			//Console.WriteLine("#--> Création des liens (Edge)");
+			//Console.WriteLine("Petite pause.....");
+			//Task.Delay(1000).Wait();
+			//loadData.CreateEdges(allJumps).Wait();
+
+			//loadData.GetRegion("The Forge").Wait();
+			//loadData.GetRegion("The Forge", 0.5).Wait();
+			//loadData.GetSystemVoisin("Jita").Wait();
+			//loadData.GetSystemVoisin("Itamo").Wait();
+			//loadData.GetItineraire("Jita", "Itamo").Wait();
+
+			Console.WriteLine("##### FIN de l'application ######");
+			Console.ReadKey();
+		}
+	}
+}
