708314d395d27ea6c497fbc19df194c19ba8467c
AnthonyRyck <anthony.ryck@hotmail.fr>
2021-10-29 15:30:18 +0200

    Recup du projet

--- src/TutoArango/ArangoGraphDemo/Program.cs
+++ src/TutoArango/ArangoGraphDemo/Program.cs
@@ -24,5 +24,59 @@ namespace ArangoGraphDemo
 			Console.WriteLine();
 			Console.WriteLine("######## Fin de l'application Démo ########");
 		}
+
+		private static ToDelete()
+		{
+			Console.WriteLine("##### Début de l'application #####");
+			Console.WriteLine("Appuyer sur une touche pour continuer...");
+			Console.ReadKey();
+
+
+			string pathBase = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files");
+
+			string pathSystemTheForge = Path.Combine(pathBase, "AllSystemsTheForge.json");
+			string pathJumpsTheForge = Path.Combine(pathBase, "AllJumpsTheForge.json");
+
+			Console.WriteLine("Chargement du fichier des systèmes solaires...");
+			string jsonContentSystems = File.ReadAllText(pathSystemTheForge);
+
+			List<SolarSystem> allSolarSystems = JsonSerializer.Deserialize<List<SolarSystem>>(jsonContentSystems);
+			Console.WriteLine($"# Il y a {allSolarSystems.Count} systèmes solaires.");
+
+			Console.WriteLine("Chargement du fichier des jumps...");
+			string jsonContentJumps = File.ReadAllText(pathJumpsTheForge);
+
+			List<Jumps> allJumps = JsonSerializer.Deserialize<List<Jumps>>(jsonContentJumps);
+			Console.WriteLine($"# Il y a {allJumps.Count} sauts interstellaires possibles.");
+
+			Console.WriteLine("Fin du chargement des fichiers...");
+
+			Console.WriteLine(":> Création de la base de donnée");
+			Console.WriteLine("Appuyer sur une touche pour continuer...");
+			Console.ReadKey();
+
+			ArangoLoader tuto = new ArangoLoader("localhost", 8529, "tutoctrlaltsuppr", "root", "CtrlAltSuppr!");
+			string nomDatabase = "CtrlAltSupprBD";
+
+			//await tuto.DropDatabase(nomDatabase);
+			//await tuto.CreateDatabase(nomDatabase);
+
+			//Console.WriteLine(":> Création d'un graph");
+			//Console.WriteLine("Appuyer sur une touche pour continuer...");
+			//Console.ReadKey();
+
+			string graphName = "EveGraph";
+			//await tuto.CreateGraph(nomDatabase, graphName);
+			//await tuto.PopulateAsync(nomDatabase, graphName, allSolarSystems, allJumps);
+
+			//Console.WriteLine("Graph créé !");
+			//Console.WriteLine(":> Faisons quelques requêtes.");
+			//Console.WriteLine("Appuyer sur une touche pour continuer...");
+			//Console.ReadKey();
+
+			await tuto.GetSystems(nomDatabase, graphName, 0.5);
+
+			Console.WriteLine("##### Fin de l'application #####");
+		}
 	}
 }
